c2e2c8e348172d0e7611635e97bf7bf2f74711c2
Chris Ash <christine.ash7@gmail.com>
2021-03-02 15:35:52 -0500

    Changed get request to use promises

--- server.js
+++ server.js
@@ -36,25 +36,20 @@ app.route('/api')
     res.json(json);
   });
 
+async function getData() {
+  const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+  const response = await fetch(url);
+  if (response.status !== 200) {
+    console.log('Houston we have a problem. Status code: ' + response.status);
+}  
+  const data = await response.json();
+  console.log('data', data); 
+}
+getData();
 
-fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
-.then(
-  function(response) {
-    if (response.status !== 200) {
-      console.log('Looks like there was a problem. Status Code: ' +
-        response.status);
-      return;
-    }
 
-    // Examine the text in the response
-    response.json().then(function(data) {
-      console.log(data);
-    });
-  }
-)
-.catch(function(err) {
-  console.log('Fetch Error :-S', err);
-});  
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
