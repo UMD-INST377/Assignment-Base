29fc39df06bcbea4d853ff54c099724bb6fa999a
Natalie Selmer <nselmer1@gmail.com>
2021-03-03 14:31:59 -0500

    assignment 1work

--- public/script.js
+++ public/script.js
@@ -1,37 +1,37 @@
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
 const cities = [];
-fetch(endpoint)
+fetch('/api')
 .then(blob => blob.json())
-.then(data => cities.push(...data))
+.then(data => cities.push(...data));
 
 fucntion findMatches(wordToMatch, cities){
     return cities.filter(place =>{
         //here we figure out if city or state matches whats searched
         const regex = new RegExp(wordToMatch, 'gi');
+        console.log('regex', regex)
         return place.city.match(regex) || place.state.match(regex)
     });
 }
 
-function numberwithCommas(x){
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-}
 
 function displayMatches(){
-    const matcArray = findMatches(this.value, cities);
+    const matchArray = findMatches(this.value, cities);
+    console.log(matchArray)
     const html = matchArray.map(place => {
         const regex = new RegExp(this.value, 'gi');
-        const cityName = place.city.replace(regex, `<span class = 'h1>${this.value}</span>`)
-        const stateName = place.state.replace(regex, `<span class = 'h1>${this.value}</span>`)
+        const cityName = place.city.replace(regex, `<span class = <'h1>${this.value}</span>`)
+        const stateName = place.state.replace(regex, `<span class = <'h1>${this.value}</span>`)
         return `
         <li>
-        <span class = 'name`>${place.city}, ${place.state}</span>
+        <span class = 'name'>${place.city}, ${place.state}</span>
         <span class = 'population'>${place.population}</span>
-        `
-        </li>
+        
+        </li> `
     }).join('');
 }
 suggestions.innerHTML = html;
-
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
-
 searchInput.addEventListener('change', displayMatches);
+window.onload = displayMatches ;
