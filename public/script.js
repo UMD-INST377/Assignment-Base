7cc02d78aec90d616c0850784add59794fa24e0c
llebedco <78024797+llebedco@users.noreply.github.com>
2021-03-03 20:56:34 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,44 +1,44 @@
 import { json } from "express";
+//<script async src="https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"></script>
+app.route('/api')
+.post(async(req, res) => {
+    console.log('POST request detected');
+    console.log('From data in res.body', req.body);
+res.send(json);
+});
+ 
+const restaurantList = document.getElementById('restaurantList');
+ let restaurant = [];
 
-<script id="myscripttag" src="https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"></script>
-    document.getElementById("myscripttag").src
+ const loadRestaurant = async () => {
+  try {
+      const res = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+      restaurant = await res.json();
+      displayRestaurant(restaurant);
+  } catch (err) {
+      console.error(err);
+  }
+};
 
-    const restaurant = [];
-    fetch(data)
-      .then(blob => blob.json())
-      .then(data => push(...data));
-      
-      app.route('/api')
-    .post(async(req, res) => {
-        console.log('POST request detected');
-        console.log('From data in res.body', req.body);
-    res.send(json);
-  });
+function displayMatches() {
+  const matchArray = findMatches(this.value, restaurant);
+  const html = matchArray.map(place => {
+    const regex = new RegExp(this.value, 'gi');
+    const restaurantName = names.restaurant.replace(regex, `<span class="hl">${this.value}</span>`);
+    const zipCode = names.zipCode.replace(regex, `<span class="hl">${this.value}</span>`);
 
-    function findMatches(wordToMatch, restaurant) {
-      return restaurant.filter(name => {
-        // here we need to figure out if the restaurant matches what was searched
-        const regex = new RegExp(wordToMatch, 'gi');
-        return name.restaurant.match(regex)
-      });
-    }
-    
-    function displayMatches() {
-      const matchArray = findMatches(this.value, restaurant);
-      const html = matchArray.map(name => {
-          return `
-          <li>
-            <span class="name">${name.restaurant}</span>
-            <span class="address">${name.address}</span>
-          </li>
-     `;   
+    return `
+      <li>
+        <span class="name">${restaurantName} ||  <span class="name">${zipCode}
+      </li>
+    `;
+  }).join('');
+  suggestions.innerHTML = html;
+  
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
 
-      }).join('');
-      suggestions.innerHTML = html;
-    }
-    
-    const searchInput = document.querySelector('.search');
-    const suggestions = document.querySelector('.suggestions');
-    
-    searchInput.addEventListener('change', displayMatches);
-    searchInput.addEventListener('keyup', displayMatches);
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
+
+     
