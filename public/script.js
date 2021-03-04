1f8d590e726b55f195bcac08c768879380eba416
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-03-04 14:31:32 -0500

    ADD async func from lecture

--- public/script.js
+++ public/script.js
@@ -1,3 +1,9 @@
+async function windowActions() {
+    console.log('window loaded');
+    const form = document.querySelector('.userform');
+    const search = document.querySelector('#zipcode')
+}
+
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 const zips = [];
          
