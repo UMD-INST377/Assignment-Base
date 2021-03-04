7866b0a3bc03c2e810fe678030720121a0e2f4ac
Chris Ash <christine.ash7@gmail.com>
2021-03-04 15:20:24 -0500

    Removed event listener on submit. Added logic to filter data. Added function to display list items

--- public/script.js
+++ public/script.js
@@ -2,23 +2,32 @@ async function windowActions() {
     console.log('window loaded');
     const form = document.querySelector('.userform');
     const search = document.querySelector('#search');
+    const suggestions = document.querySelector('.suggestions');
 
     const request = await fetch('/api')
     const data = await request.json();
 
-    form.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        console.log('submit fired', search.value);
-        const display = data.filter((record) => record.city.toUpperCase() === search.value.toUpperCase() || record.zip.toUpperCase() === search.value.toUpperCase());
-        console.log(display);
-        console.table(display);
-
-    });
     search.addEventListener('input', (event) => {
-        console.log('input', event.target.value);
-    });
+        console.log('input', event.target.value)
+        const display = data.filter((record) => {
+            return record.city.toUpperCase().includes(event.target.value.toUpperCase()) || record.zip.toUpperCase().includes(event.target.value.toUpperCase())
+        });
 
-}
+        display.forEach(restaurant => {
+            const newItem = document.createElement('li');
+            newItem.classList.add('list-item');
+            newItem.innerHTML = `
+            <strong>${restaurant.name}</strong>
+            ${restaurant.category}
+            ${restaurant.address_line_1}
+            ${restaurant.city}
+            ${restaurant.zip}
+            `;
+            
+            suggestions.append(newItem);
+        });
+    })
+};
 
 window.onload = windowActions;
 
