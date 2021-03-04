24f2edf1123379b3b2f2ce1efc6872027431b88b
llebedco <78024797+llebedco@users.noreply.github.com>
2021-03-04 09:39:47 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,44 +1,17 @@
-import { json } from "express";
-//<script async src="https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"></script>
-app.route('/api')
-.post(async(req, res) => {
-    console.log('POST request detected');
-    console.log('From data in res.body', req.body);
-res.send(json);
-});
  
-const restaurantList = document.getElementById('restaurantList');
- let restaurant = [];
+async function windowAction() {
+  const form = document.querySelector('.userform');
+  const search = document.querySelector('#city');
 
- const loadRestaurant = async () => {
-  try {
-      const res = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
-      restaurant = await res.json();
-      displayRestaurant(restaurant);
-  } catch (err) {
-      console.error(err);
-  }
-};
+  const request = await fetch('/api');
+  const data = await request.json();
 
-function displayMatches() {
-  const matchArray = findMatches(this.value, restaurant);
-  const html = matchArray.map(place => {
-    const regex = new RegExp(this.value, 'gi');
-    const restaurantName = names.restaurant.replace(regex, `<span class="hl">${this.value}</span>`);
-    const zipCode = names.zipCode.replace(regex, `<span class="hl">${this.value}</span>`);
+  form.addEventListener('submit', async(event) => {
+    event.preventDefault();
+    const display = data.filter((record) => record.city.toUpperCase() === search.nodeValue.toUpperCase());
+  });
+  search.addEventListener('input', (event) => {
+    console.log('input', event.target.value);
+  });
+} 
 
-    return `
-      <li>
-        <span class="name">${restaurantName} ||  <span class="name">${zipCode}
-      </li>
-    `;
-  }).join('');
-  suggestions.innerHTML = html;
-  
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
-
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
-
-     
