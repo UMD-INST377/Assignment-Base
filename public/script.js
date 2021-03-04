d593c462387ce0b8170edd2a8b4f5f89be199c7c
Emmet Ryan <emmetryan7@gmail.com>
2021-03-04 14:00:13 -0500

    Removed unnecessary code

--- public/script.js
+++ public/script.js
@@ -1,26 +1,26 @@
-results = [];
-
-const searchInput = document.querySelector("#search");
-const suggestions = document.querySelector(".suggestions");
-
 function findMatches(wordToMatch, results) {
   return results.filter((location) => {
     const regex = new RegExp(wordToMatch, "gi");
-    return location.city.match(regex) || location.category.match(regex) || location.name.match(regex);
+    return (
+      location.city.match(regex) ||
+      location.category.match(regex) ||
+      location.name.match(regex)
+    );
   });
 }
 
-async function windowActions(){
-  const form = document.querySelector(".form")
-  const search = document.querySelector("#search")
+async function windowActions() {
+  const search = document.querySelector("#search");
+  const suggestions = document.querySelector(".suggestions");
 
-  const request = await fetch('/api');
+  const request = await fetch("/api");
   const results = await request.json();
 
-  search.addEventListener("keyup", (event) =>{
-    const matchArray = findMatches(search.value, results)
-    const html = matchArray.map(location => {
-      return `
+  search.addEventListener("keyup", (event) => {
+    const matchArray = findMatches(search.value, results);
+    const html = matchArray
+      .map((location) => {
+        return `
         <li>
             <span class="name">${location.name}</span>
             <span class="category">${location.category}</span>
@@ -29,8 +29,10 @@ async function windowActions(){
             <span class="zip">${location.zip}</span>
         </li>
       `;
-  }).join('');
-  suggestions.innerHTML = html;
+      })
+      .join("");
+    suggestions.innerHTML = html;
   });
 }
+
 window.onload = windowActions();
