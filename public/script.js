26ec6e8b57f058ce7d76467ded49d726792d365a
gar622 <73448556+gar622@users.noreply.github.com>
2021-03-04 20:39:43 -0700

    commit 3
    update to css, script

--- public/script.js
+++ public/script.js
@@ -1,3 +1,26 @@
+const { default: fetch } = require("node-fetch");
+
+async function windowActions(){
+    console.log('window loaded');
+    const form = document.querySelector('.userform')
+    const search = document.querySelector('#type'); // idk if the database calls the type of restaurant?
+
+    const request = await fetch('/api');
+    const data = await request.json;
+    
+    form.addEventListener(async (event)=> {
+        event.preentDefault();
+        console.log('submit fired', search.value);
+        const display = data.filter((record) => record.type.toUpperCase() === search.value.toUpperCase());
+
+    });
+    search.addEventListener('input', (event) =>{
+        console.log('input', event.target.value);
+    });
+}
+window.onload=windowActions;
+
+
 // const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 // const restaurants = [];
 
