dde5e2c92f09cfa810b2e1b058f682e7bd236aea
llebedco <78024797+llebedco@users.noreply.github.com>
2021-03-04 22:20:37 -0500

    Update script.js
    final working code

--- public/script.js
+++ public/script.js
@@ -1,62 +1,29 @@
-app.route('/api')
-.post(async(req, res) => {
-    console.log('POST request detected');
-    console.log('From data in res.body', req.body);
-res.send(json);
-});
- 
-const restaurantList = document.getElementById('restaurantList');
- let restaurant = [];
+let restaurantList = []
+const searchInput = document.querySelector('.search');
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
 
- async function windowAction() {
-   const form = document.querySelector('userform');
-   const search = document.querySelector('#city');
+async function getData() {
+  const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+  const json = await data.json();
+  restaurantList = json
+}
+window.onload = getData
 
-   const request = await fetch('/api');
-   const data = await request.json();
-  
-   form.addEventListener('submit' async(event) => {
-     event.preventDefault();
-     const display = data.filter((record) => record.restaurant)
-   })
- }
+// here we need to figure out if the restaurant matches what was searched
+function findMatches(word) {
+  return restaurantList.filter(restaurant => restaurant.name.toLowerCase().indexOf(word) > -1);
+}
 
-     
 function displayMatches() {
-  const matchArray = findMatches(this.value, restaurant);
-  const html = matchArray.map(place => {
-    const regex = new RegExp(this.value, 'gi');
-    const restaurantName = names.restaurant.replace(regex, `<span class="hl">${this.value}</span>`);
-    const zipCode = names.zipCode.replace(regex, `<span class="hl">${this.value}</span>`);
-
-    function findMatches(wordToMatch, restaurant) {
-      return restaurant.filter(name => {
-        // here we need to figure out if the restaurant matches what was searched
-        const regex = new RegExp(wordToMatch, 'gi');
-        return name.restaurant.match(regex)
-      });
-    }
-    
-    function findMatches(word) {
-      return restaurantList.filter(restaurant => restaurant.name.toLowerCase().indexOf(word) > -1);
-    }
-    
-    function displayMatches() {
-      const matchArray = findMatches(this.value);
-      const html = matchArray.map(restaurant => {
-        return `<li class="">
-                <div class="name">${restaurant.name}</div>
-                <div class="text">${restaurant.category}</div>
-                <div class="text italic">${restaurant.address_line_1}</div>
-                <div class="text italic">${restaurant.city}</div>
-                <div class="text italic">${restaurant.zip}</div>
-              </li>`;
-      }).join('');
-      suggestions.innerHTML = html;
-  
-  
-
- 
- 
- 
-     
+  const matchArray = findMatches(searchInput.value);
+  document.querySelector('.suggestions').innerHTML = matchArray.map(restaurant => {
+    return `<li class="">
+            <div class="name">${restaurant.name}</div>
+            <div class="text">${restaurant.category}</div>
+            <div class="text italic">${restaurant.address_line_1}</div>
+            <div class="text italic">${restaurant.city}</div>
+            <div class="text italic">${restaurant.zip}</div>
+          </li>`;
+  }).join('');
+}
