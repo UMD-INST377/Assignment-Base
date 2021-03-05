aced40961e35a9f66061a947511fc5936535d349
Jonghyeong Kim <aengida@gmail.com>
2021-03-05 01:23:18 -0500

    update3 js

--- public/script.js
+++ public/script.js
@@ -1,42 +1,52 @@
 async function windowsActions() {
+    console.log('window loaded')
     const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
     const request = await fetch(endpoint);
-    const arrayName = await request.json();
+    const data = await request.json();
+    const restaurants = []
 
     function findMatches(wordToMatch, restaurants){
+        console.log('find data')
         return restaurants.filter(place => {
             const regex = new RegExp(wordToMatch, 'gi');
             return place.city.match(regex) || place.state.match(regex);
+            
         });
     }
-    function numberWithCommas(x) {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-    }
+
+    // function numberWithCommas(x) {
+    //     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    // }
 
     function displayMatches(event){
-        const matchArray = findMatches(event.target.value, types);
+        console.log('display matches')
+        const matchArray = findMatches(event.target.value, restaurants);
         const html = matchArray.map(place => {
             const regex = new RegExp(event.target.value, 'gi');
-            const TypeRest = place.category.replace(regex, 
+            const CategoryName = place.category.replace(regex, 
                 `<span class="hl">${event.target.value}</span>`);
             const RestName = place.name.replace(regex, 
-                `<span class="hl">${event.target.value}</span>`);     
+                `<span class="hl">${event.target.value}</span>`);
             return `
                 <li>
-                    <span class="name>${TypeRest}, ${RestName}</span>
-                    <span class="population">${numberWithCommas(place.population)}</span>
+                    <span class="name">${CategoryName}, ${RestName}</span>
+                    <span class="address">${place.address_line_1}</span>
+                    <span class="address">${place.address_line_2}</span>
+                    <span class="city">${place.city}</span>
+                    <span class="state">${place.state}</span>
+                    <span class="zipcode">${place.zip}</span>
+                    <span class="category">${place.category}</span>
                 <li>
             `;
         }).join('');
         suggestions.innerHTML = html;
     }
 
-    const searchInput = document.querySelector('#name');
+    const searchInput = document.querySelector('.userform');
     const suggestions = document.querySelector('.suggestions');
-    searchInput.addEventListener('keyup', async (evt) => {
-        displayMatches(evt);
-        evt.preventDefault();
-    });
 
+    searchInput.addEventListener('keyup', displayMatches);
+    searchInput.addEventListener('change', displayMatches);
 }
-window.onload = windowActions;
+
+window.onload = windowsActions;
--- public/script1.js
+++ public/script1.js
@@ -1,21 +1,23 @@
-const { default: fetch } = require("node-fetch");
-
 async function windowActions(){
     console.log('window loaded');
     const form = document.querySelector('.userform')
-    const search = document.querySelector('#type'); // idk if the database calls the type of restaurant?
+    const search = document.querySelector('.category');
+    
 
-    const request = await fetch('/api');
+    const request = await fetch(https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
     const data = await request.json;
     
+    search.addEventListener('input', (event) =>{
+        console.log('input', event.target.value);
+    });
+
     form.addEventListener(async (event)=> {
-        event.preentDefault();
+        event.preventDefault();
         console.log('submit fired', search.value);
         const display = data.filter((record) => record.type.toUpperCase() === search.value.toUpperCase());
 
     });
-    search.addEventListener('input', (event) =>{
-        console.log('input', event.target.value);
-    });
+
+
 }
 window.onload=windowActions;
