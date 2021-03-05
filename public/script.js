71236f2e1bce8db96871c4edf5b6b83d0d66e2bc
Natalie Selmer <nselmer1@gmail.com>
2021-03-05 12:00:11 -0500

    added a reducer

--- public/script.js
+++ public/script.js
@@ -8,34 +8,42 @@ fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
     return blob.json();
 })
 .then(function(data) {
-    places = data;
+ places = data;
 })
+
+/*async function windowActions() {
+   console.log('window loaded');
+const request = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+const places = await request.json([0]);
+};*/
+
 
 function displayMatches(e){
     e.preventDefault();
 
-    const matchArray = places.filter(function(place) {
+    const matchArray = places.filter(function(place){
+        
         return place.city.includes(searchInput.value) || place.zip.includes(searchInput.value);
     });
 
 
     console.log(matchArray);
 
-    let HTML = "";
-    matchArray.forEach(function(place) {
-        HTML += `
+const HTML = matchArray.reduce((string, place, index) => {
+        string += `
             <li class="box">
-                <div class='name'>${place.name}</div>
-                <div class='name'>${place.category}</div>
-                <div class='name'>${place.address_line_1}</div>
-                <div class='name'>${place.city}</div>
-                <div class='name'>${place.zip}</div>
+                <div class='name'>${place.name.toUpperCase()}</div>
+                <div class='category'>${place.category.toUpperCase()}</div>
+                <div class='address'>${place.address_line_1.toUpperCase()}</div>
+                <div class='city'>${place.city.toUpperCase()}</div>
+                <div class='zip'>${place.zip}</div>
 
             </li>
-        `;
-    });
+        `
+    return string}, '');
    
     suggestions.innerHTML = HTML;
-
+console.log(HTML)
 }
 document.querySelector("form").addEventListener('submit', displayMatches);
+//window.onload=windowActions();
