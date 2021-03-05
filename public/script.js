bf2e91f8a6a1c28641926f17f3d360d0fa4788a2
gar622 <73448556+gar622@users.noreply.github.com>
2021-03-04 21:09:35 -0700

    commit

--- public/script.js
+++ public/script.js
@@ -1,28 +1,7 @@
 const { default: fetch } = require("node-fetch");
 
-async function windowActions(){
-    console.log('window loaded');
-    const form = document.querySelector('.userform')
-    const search = document.querySelector('#type'); // idk if the database calls the type of restaurant?
-
-    const request = await fetch('/api');
-    const data = await request.json;
-    
-    form.addEventListener(async (event)=> {
-        event.preentDefault();
-        console.log('submit fired', search.value);
-        const display = data.filter((record) => record.type.toUpperCase() === search.value.toUpperCase());
-
-    });
-    search.addEventListener('input', (event) =>{
-        console.log('input', event.target.value);
-    });
-}
-window.onload=windowActions;
-
-
-// const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-// const restaurants = [];
+const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+const restaurants = [];
 
     function numberWithCommas(x) {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
