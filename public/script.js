cd451fb50a8bc14aa7116df0746914af921bd98b
alechacon99 <77997217+alechacon99@users.noreply.github.com>
2021-03-05 22:00:27 -0500

    assignment updates

--- public/script.js
+++ public/script.js
@@ -1,10 +1,9 @@
+async function windowActions() {
+console.log('Window loaded')
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-
-const restaurants = [];
-
-fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => restaurants.push(...data))
+const restaurants = []
+const request = await fetch(endpoint)
+const request_data = await request.json().then(data => restaurants.push(...data));
 
 function findMatches(wordToMatch, restaurants){
     return restaurants.filter(place => {
@@ -13,12 +12,13 @@ function findMatches(wordToMatch, restaurants){
     });
 };
 
-function displayMatches(){
-    const matchArray = findMatches(this.value, restaurants);
+function displayMatches(event){
+    const search_input = event.target.value
+    const matchArray = findMatches(event.target.value, restaurants);
     const html = matchArray.map(place => {
-        const regex = new RegExp(this.value, 'gi');
-        const nameName = place.name.replace(regex, `<span class="h1">${this.value}</span>`);
-        const categoryName = place.category.replace(regex, `<span class="h1">${this.value}</span>`);
+        const regex = new RegExp(event.target.value, 'gi');
+        const nameName = place.name.replace(regex, `<span class="h1">${event.target.value}</span>`);
+        const categoryName = place.category.replace(regex, `<span class="h1">${event.target.value}</span>`);
         return `
         <li class="box has-background-success-light"> 
             <span class="name">${nameName}</span><br>
@@ -27,11 +27,20 @@ function displayMatches(){
         </li>
         `;
     }).join('');
-    suggestions.innerHTML = html;
+
+    if(search_input){
+        suggestions.innerHTML = html; //takes the html strong from html and creates html in this element
+    }else {
+        suggestions.innerHTML = "";
+    };
 };
 
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
 
 searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
+
+};
+
+window.onload = windowActions
