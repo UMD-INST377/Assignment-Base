4666b825849532cae353729c176db1f8befd8ff7
Tahmed15 <78212428+Tahmed15@users.noreply.github.com>
2021-03-05 23:03:30 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,9 +3,15 @@ async function windowActions() {
     console.log('window loaded');
     const form = document.querySelector('.userform');
     const search = document.querySelector('#zipcode')
+<<<<<<< HEAD
     const suggestions = document.querySelector('.suggestions');
     
     const request = await fetch('/api');
+=======
+    const targetList = document.querySelector('.target-list');
+
+    const request = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+>>>>>>> aa5f99a02f73b398e522a9fc6430af09a16b47e3
     const data = await request.json();
 
     search.addEventListener('input', (event)=>{
@@ -13,6 +19,7 @@ async function windowActions() {
         const display = data.filter(record) =>{
             return record.city.toUpperCase().includes(event.target.value.toUpperCase() || record.zip.includes(event.target.value);
         });
+<<<<<<< HEAD
 
         display.forEach(restaurant => {
             const newItem = document.createdElement('li');
@@ -30,6 +37,34 @@ async function windowActions() {
     })
                                                                                                   
     
+=======
+
+        // filter through each restaurant to add its name, cat, add, and zip into the list
+        function displayMatches() {
+            filtered.forEach(restaurant => {
+                const newItem = document.createElement('li');
+                newItem.classList.add('list-item');
+                newItem.innerHTML = `
+                <h2>${restaurant.name}</h2>
+                <h3>${restaurant.category}</h3>
+                ${restaurant.address_line_1}
+                ${restaurant.zip}
+                `;
+                targetList.append(newItem);
+            }); 
+        }
+    })   
+
+    // this listens for typing into the input box
+    search.addEventListener('input', (event) => {
+        console.log('input', event.target.value);
+    });
+    
+    search.addEventListener('change', displayMatches);
+    search.addEventListener('keyup', (event) => {
+        displayMatches(event);
+    })
+>>>>>>> aa5f99a02f73b398e522a9fc6430af09a16b47e3
 };
 
 window.onload = windowActions;
